9de216d6525382ddbe79a398c26f9dd3a8882c4f
aliciaajy <65158683+aliciaajy@users.noreply.github.com>
2021-03-14 17:23:55 +0800

    created login and routes

---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+       <img src="../assets/earth.png" class = "image">
+    </div>
+</template>
+
+<script>
+
+export default {    
+}
+
+</script>
+
+<style scoped>
+
+.image {
+    width: 500px;
+    height: 500px;
+    position: absolute;
+    left: 0px;
+}
+
+</style>
