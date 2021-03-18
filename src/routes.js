4e9bf606e9dbb8804f00da29ae0b8272e2b589c1
aliciaajy <65158683+aliciaajy@users.noreply.github.com>
2021-03-18 14:26:02 +0800

    added specific mart folder

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import Login from './components/login/Login.vue'
 import SignUp from './components/login/SignUp.vue'
 import ForgotPassword from './components/login/ForgotPassword.vue'
-import MartsPage from './components/mart/martsPage.vue'
+import searchPage from './components/search/searchPage.vue'
 import Home from './components/home/Home.vue'
 
 export default [
@@ -9,6 +9,6 @@ export default [
     { path: '/login', component: Login },
     { path: '/forgot-password', component: ForgotPassword },
     { path: '/home', component: Home },
-    { path: '/search', component: MartsPage },
+    { path: '/search', component: searchPage },
    
 ]
