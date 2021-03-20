d6ccb05b2e2c87c28b92b4b1415d45cb3a0dad57
aliciaajy <65158683+aliciaajy@users.noreply.github.com>
2021-03-21 01:13:59 +0800

    added navi tab to item

--- src/components/items/itemsPage.vue
+++ src/components/items/itemsPage.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
     <Bar></Bar>
-      
+    <button class="tablink" onclick="openPage('Home', this, 'red')">
+      Your Items
+    </button>
+    <button
+      class="tablink"
+      onclick="openPage('News', this, 'green')"
+      id="defaultOpen"
+    >
+      Items Expiring Soon
+    </button>
+    <button class="tablink" onclick="openPage('Contact', this, 'blue')">
+      Items Expired
+    </button>
   </div>
 </template>
 
@@ -9,9 +21,55 @@
 
 
 <script>
-
-export default {
-
-}
+export default {};
 </script>
 
+<style scoped>
+/* Set height of body and the document to 100% to enable "full page tabs" */
+body,
+html {
+  height: 100%;
+  margin: 0;
+  font-family: Arial;
+}
+
+/* Style tab links */
+.tablink {
+  background-color: rgb(160, 189, 187);
+  color: rgb(255, 255, 255);
+
+  float: left;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 44px 10px;
+  font-size: 20px;
+  width: 33.31%;
+}
+
+.tablink:hover {
+  background-color: #777;
+}
+
+/* Style the tab content (and add height:100% for full page content) */
+.tabcontent {
+  color: white;
+  display: none;
+  padding: 100px 20px;
+  height: 100%;
+
+}
+
+#Home {
+  background-color: red;
+}
+#News {
+  background-color: green;
+}
+#Contact {
+  background-color: blue;
+}
+#About {
+  background-color: orange;
+}
+</style>
